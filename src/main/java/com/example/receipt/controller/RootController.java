93b1ddf9097cb565304532cff0a947716d4c0cad
vineethreddybh <vineethreddy1568@gmail.com>
2025-04-13 20:36:42 -0400

    Fixed Errors

---
+++ src/main/java/com/example/receipt/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.example.receipt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Receipt Processor is up!";
+    }
+}
